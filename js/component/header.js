aa261c472d2fc75ee8fa6241aca41d34a35e8e8d
jonzoid <jonzoid1@gmail.com>
2015-02-01 22:08:10 -0500

    Shrunk header slightly

--- js/component/header.js
+++ js/component/header.js
@@ -6,7 +6,11 @@ $.inherits(shed.component.header, shed.component);
 shed.component.header.prototype.decorate_ = function(parent) {
   var stonehearth_logo = $.createElement('img')
     .setAttribute('src', 'img/stonehearth_logo.png')
-    .style({'display': 'block', 'margin': '20px auto 20px auto'});
+    .style({
+      'display': 'block',
+      'margin': '20px auto 20px auto',
+      'width': '600px'
+    });
 
   parent.appendChild(stonehearth_logo);
 };
